d6559918f2ea5375ddb4b5abd26c94328047ca7f
Vegard Lillesalt Valvik <vegard.valvik@gmail.com>
2022-06-01 14:35:25 +0200

    Cleanup in html and js

--- index.js
+++ index.js
@@ -2,11 +2,6 @@ const passwordLength = 15;
 const numPasswords = 4;
 const passwordEls = document.querySelectorAll('.password')
 
-let passwordEl1 = document.getElementById("password-el-1");
-let passwordEl2 = document.getElementById("password-el-2");
-let passwordEl3 = document.getElementById("password-el-3");
-let passwordEl4 = document.getElementById("password-el-4");
-
 let passwordString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890()&*!$abcdefghijklmnopqrstuvwxyz"
 
 function generatePassword() {
