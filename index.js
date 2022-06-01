63c0ba71dd63d6170153e7232fd0c7ea8fa1985c
Vegard Lillesalt Valvik <vegard.valvik@gmail.com>
2022-06-01 15:02:01 +0200

    Code cleanup, change password functions

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 const passwordLength = 15;
 const numPasswords = 4;
 const passwordEls = document.querySelectorAll('.password')
-
 let passwordString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890()&*!$abcdefghijklmnopqrstuvwxyz"
+let passwords = [];
 
-function generatePassword() {
-	let randomPassword = "";
-	for (let i = 0; i < passwordLength; i++) {
-		let randomNumber = Math.floor( Math.random() * (passwordString.length -1) +1)
-		randomPassword += passwordString[randomNumber];
+// Generate passwords and store them in an array
+function generatePasswords() {
+	for (let i = 0; i < numPasswords; i++) {
+		let randomPassword = "";
+		for (let i = 0; i < passwordLength; i++) {
+			let randomNumber = Math.floor( Math.random() * (passwordString.length -1) +1)
+			randomPassword += passwordString[randomNumber];
+		}
+		passwords[i] = randomPassword;
 	}
-	return randomPassword;
 }
 
+// Button listener triggered -> assign all passwordEls with content from password array
 function generateButtonClicked() {
+	generatePasswords();
+
 	for (i = 0; i<numPasswords; i++) {
-		passwordEls[i].textContent = generatePassword();
+		passwordEls[i].textContent = passwords[i];
 	}
 }
