2525eca8151fe25f5841d3bdfa5ba04742097cf1
Vegard Lillesalt Valvik <vegard.valvik@gmail.com>
2022-06-01 15:46:09 +0200

    Rewrite generatePassword function (again)

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
 const passwordLength = 15;
 const numPasswords = 4;
 const passwordEls = document.querySelectorAll('.password')
-let passwordString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890()&*!$abcdefghijklmnopqrstuvwxyz"
-let passwords = [];
 
-// Generate passwords and store them in an array
-function generatePasswords() {
-	for (let i = 0; i < numPasswords; i++) {
-		let randomPassword = "";
-		for (let i = 0; i < passwordLength; i++) {
-			let randomNumber = Math.floor( Math.random() * (passwordString.length -1) +1)
-			randomPassword += passwordString[randomNumber];
-		}
-		passwords[i] = randomPassword;
+const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numbers = "0123456789";
+const symbols = "!#$&^*";
+
+
+// Generate password by populating an array with all possible characters and traversing with a random number
+function generatePassword() {
+	let randomPassword = "";
+	let characters = lowercaseChars + uppercaseChars + numbers + symbols;
+	characters = characters.split('');
+
+	for (let i = 0; i < passwordLength; i++) {
+		let randomNumber = Math.floor( Math.random() * (characters.length - 1) +1);
+		randomPassword += characters[randomNumber];
 	}
+return randomPassword;
 }
 
-// Button listener triggered -> assign all passwordEls with content from password array
+// Button listener triggered -> assign all passwordEls with password from generatePassword function
 function generateButtonClicked() {
-	generatePasswords();
-
-	for (i = 0; i<numPasswords; i++) {
-		passwordEls[i].textContent = passwords[i];
+	for (i = 0; i < numPasswords; i++) {
+		passwordEls[i].textContent = generatePassword();
 	}
 }
